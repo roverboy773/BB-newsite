7dc54c9408801ebe4bcf5ec27cf7f1a8ec03a409
aashika_mittal <aashikamittal999@gmail.com>
2021-09-03 19:17:03 +0530

    not done

--- js/home.js
+++ js/home.js
@@ -42,39 +42,9 @@ popular.addEventListener("click", (e) => {
 
 });
 
-// ---------------TESTIMONIAL PURPLE BUTTON CHANGE------------//
-const testimonial_button1=document.querySelector('.testimonial_button1')
-const testimonial_button2=document.querySelector('.testimonial_button2')
 
-
-let currSlide=2;
-let prevSlide=2;
-
-testimonial_button1.addEventListener('click',()=>{
-    // console.log(';clciked')
-    document.getElementById(`purple_square${prevSlide}`).style.backgroundColor="#634ABB"
-    document.getElementById(`purple_square${prevSlide}`).style.transform="scale(1)"
-
-
-    currSlide--;
-   if(currSlide<1)
-   currSlide=4;
-   prevSlide=currSlide
-
-   document.getElementById(`purple_square${currSlide}`).style.backgroundColor="#FCA702"
-   document.getElementById(`purple_square${currSlide}`).style.transform="scale(1.75)"
-})
-testimonial_button2.addEventListener('click',()=>{
-    // console.log(';clciked')
-    document.getElementById(`purple_square${prevSlide}`).style.backgroundColor="#634ABB"
-    document.getElementById(`purple_square${currSlide}`).style.transform="scale(1)"
-
-    currSlide++;
-   if(currSlide>4)
-   currSlide=currSlide%4;
-   prevSlide=currSlide
-
-   document.getElementById(`purple_square${currSlide}`).style.backgroundColor="#FCA702"
-   document.getElementById(`purple_square${currSlide}`).style.transform="scale(1.75)"
-})
-
+window.onscroll = function(e) {
+    // print "false" if direction is down and "true" if up
+    console.log(e+ new Date());
+    this.oldScroll = this.scrollY;
+  }
